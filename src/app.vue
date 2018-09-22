4f2a3ec7e379cafbdc29d4f1be781f6ae9dbd828
Kodedly <meetenebelioluchi@gmail.com>
2018-09-22 17:07:40 +0100

    initial commit of repository

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template >
+  <div id="app">
+    <img src="./assets/hello.jpeg">
+    <h1>This is a skeleton application for vue based projects.</h1>
+  </div>
+</template>
+
+<!-- App specific objects  -->
+<script type="text/javascript">
+  module.exports = {
+    name: "app"
+  }
+</script>
+
+<!-- App specific stylings  -->
+<style media="screen">
+  #app {
+    
+  }
+</style>
